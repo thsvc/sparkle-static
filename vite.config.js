f75ea74b82fefe6cea43ccc24a91c9aefe1c2c27
thsvc <100921991+thsvc@users.noreply.github.com>
2025-08-26 18:28:02 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  base: "/sparkle-static/"
+  base: "/", // domaine custom => racine
 });
